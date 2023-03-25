14fbbe1a9f10b93fe3bb856b30a753269fa51b05
ThatDRW <d.renswoude@gmail.com>
2023-03-25 17:16:41 +0100

    Impl: Landing Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 const routes: Routes = [
+  { path: '', component: LandingComponent },
   { path: 'user/register', component: RegisterUserComponent }
 ];
 
