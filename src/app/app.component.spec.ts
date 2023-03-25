428c7eed392cfc1fdba4b6284cb895ba6d95dddc
ThatDRW <d.renswoude@gmail.com>
2023-03-25 12:05:47 +0100

    Fix: Project Renamed.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'customerdetailsservice-angular'`, () => {
+  it(`should have as title 'angular-customerdetailsservice'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('customerdetailsservice-angular');
+    expect(app.title).toEqual('angular-customerdetailsservice');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('customerdetailsservice-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-customerdetailsservice app is running!');
   });
 });
