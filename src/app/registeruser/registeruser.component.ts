ec546b3b0a09bbb99c5e7d4fa5ddaf6731ba3588
ThatDRW <d.renswoude@gmail.com>
2023-03-25 13:17:27 +0100

    Feat: Added Bootstrap and JQuery.

---
+++ src/app/registeruser/registeruser.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registeruser',
+  templateUrl: './registeruser.component.html',
+  styleUrls: ['./registeruser.component.css']
+})
+export class RegisteruserComponent {
+
+  student = "Student Boogedy reporting for doogedy.";
+
+}
