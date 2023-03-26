8d2095116b800cb930dc310d7148b9cefd97d70c
ThatDRW <d.renswoude@gmail.com>
2023-03-26 17:18:15 +0200

    Impl: Added Login Route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterUserComponent } from './components/register-user/register-user.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { LoginUserComponent } from './components/login-user/login-user.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'user/register', component: RegisterUserComponent }
+  { path: 'user/register', component: RegisterUserComponent },
+  { path: 'login', component: LoginUserComponent }
 ];
 
 @NgModule({
