2f680aa9f41e0198e2482ec6365477421affdebc
ThatDRW <d.renswoude@gmail.com>
2023-03-26 17:00:23 +0200

    Fix: Fixed imports.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from '../entity/User';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+    private baseUrl = "http://localhost:8080/user"
     private userRegisterUrl = "http://localhost:8080/user/register";
 
     constructor(private http: HttpClient) { }
 
-    registerUser(user: User): Observable<User> {
-        return this.http.post<User>(this.userRegisterUrl, user);
+    registerUser(data: any): Observable<User> {
+        return this.http.post<User>(this.userRegisterUrl, data);
     }
 }
