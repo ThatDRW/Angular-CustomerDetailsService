95363da47bc1f0d91f833fff161e33a252ee1a8a
ThatDRW <d.renswoude@gmail.com>
2023-03-26 13:18:01 +0200

    Impl: Register user function.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../entity/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+    private userRegisterUrl = "http://localhost:8080/user/register";
+
+    constructor(private http: HttpClient) { }
+
+    registerUser(user: User): Observable<User> {
+        return this.http.post<User>(this.userRegisterUrl, user);
+    }
 }
