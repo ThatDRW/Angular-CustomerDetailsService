515d0b8622cff21fe8f94fef3a32269da9a1d90e
ThatDRW <d.renswoude@gmail.com>
2023-03-27 15:33:40 +0200

    Impl: logout route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterUserComponent } from './components/register-user/register-user.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginUserComponent } from './components/login-user/login-user.component';
+import { LogoutUserComponent } from './components/logout-user/logout-user.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'user/register', component: RegisterUserComponent },
-  { path: 'login', component: LoginUserComponent }
+  { path: 'login', component: LoginUserComponent },
+  { path: 'logout', component: LogoutUserComponent }
 ];
 
 @NgModule({
