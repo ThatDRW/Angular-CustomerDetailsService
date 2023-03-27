7d9d526d17578545399793f36d1989b86848ec20
ThatDRW <d.renswoude@gmail.com>
2023-03-27 14:45:16 +0200

    Impl: Basic isLoggedIn functionality.

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from '../services/token-storage.service';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.css']
 })
-export class NavigationBarComponent {
+export class NavigationBarComponent implements OnInit {
+
+    constructor(
+        private tokenStorage: TokenStorageService
+    ) { }
+
+    isLoggedIn = false;
+
+    ngOnInit(): void {
+        if (this.tokenStorage.getToken()) {
+            this.isLoggedIn = true;
+        }
+    }
 
 }
