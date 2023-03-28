cb9507d9195874ea41a6d13b4af81ffb8051674d
ThatDRW <d.renswoude@gmail.com>
2023-03-28 12:51:51 +0200

    Impl: Detail view and Editing functionality.

--- src/app/components/details-customer/details-customer.component.ts
+++ src/app/components/details-customer/details-customer.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Customer } from 'src/app/models/customer.model';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-details-customer',
   templateUrl: './details-customer.component.html',
   styleUrls: ['./details-customer.component.css']
 })
-export class DetailsCustomerComponent {
+export class DetailsCustomerComponent implements OnInit {
 
+    customer:Customer = {
+        id: undefined,
+        firstname: undefined,
+        lastname: undefined,
+        age: undefined,
+        address: undefined
+    };
+
+    getId = '';
+    hasError = false;
+    errorMessage = '';
+    isViewingCustomer = false;
+    isEditing = false;
+    submitted = false;
+
+
+    constructor(private customerService: CustomerService) { }
+
+    ngOnInit() : void {
+
+    }
+
+    getCustomer() : void {
+        this.customerService.getCustomer(this.getId)
+        .subscribe({
+            next: (res) => {
+                console.log(res);
+
+                this.hasError = false;
+                this.errorMessage = '';
+                this.isViewingCustomer = true;
+                this.customer = CustomerService.custofyData(res);
+                this.submitted = false;
+            },
+            error: (e) => {
+                console.error(e);
+                console.log(e.error.status + " " + e.error.title);
+
+                this.hasError = true;
+                this.errorMessage = e.error.status + " " + e.error.title;
+            }
+        });
+    }
+
+    editCustomer() : void {
+        this.isEditing = true;
+    }
+
+    saveCustomer() : void {
+        const data = CustomerService.datafyCustomer(this.customer, true);
+
+        this.customerService.addCustomer(data)
+        .subscribe({
+            next: (res) => {
+                console.log(res);
+                this.submitted = true;
+                this.isEditing = false;
+                this.getCustomer();
+            },
+            error: (e) => {
+                console.error(e);
+                this.hasError = true;
+                console.log(e.error.status + " " + e.error.title);
+                this.errorMessage = e.error.status + " " + e.error.title;
+            }
+        });
+    }
+
+    cancelEdit() : void {
+        this.getCustomer();
+        this.isEditing = false;
+    }
 }
