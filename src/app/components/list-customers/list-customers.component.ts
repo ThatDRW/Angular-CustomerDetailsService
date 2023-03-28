3c2977c482eb1808dd649b6dc91194d4fef3d254
ThatDRW <d.renswoude@gmail.com>
2023-03-28 13:40:45 +0200

    Impl: AllCustomers call.

--- src/app/components/list-customers/list-customers.component.ts
+++ src/app/components/list-customers/list-customers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
@@ -10,10 +10,34 @@ export class ListCustomersComponent implements OnInit {
 
     customerList = undefined;
 
+    hasError = false;
+    errorMessage = '';
+
+    @Input()
+    viewAll = true;
+
     constructor(private customerService : CustomerService) { }
 
     ngOnInit(): void {
+        if (this.viewAll) {
+            this.customerService.getAllCustomers()
+            .subscribe({
+                next: (res) => {
+                    console.log(res);
 
+                    this.hasError = false;
+                    this.errorMessage = '';
+                    this.customerList = res;
+                },
+                error: (e) => {
+                    console.error(e);
+                    console.log(e.error.status + " " + e.error.title);
+
+                    this.hasError = true;
+                    this.errorMessage = e.error.status + " " + e.error.title;
+                }
+            });
+        }
     }
 
     getAllCustomers() : void {
