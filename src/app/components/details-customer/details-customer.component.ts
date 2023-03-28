abc3e0f6c4dba4daae067eb04eaef9828bfc8755
ThatDRW <d.renswoude@gmail.com>
2023-03-28 11:30:20 +0200

    Feat: DetailsCustomer Component added.

---
+++ src/app/components/details-customer/details-customer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-customer',
+  templateUrl: './details-customer.component.html',
+  styleUrls: ['./details-customer.component.css']
+})
+export class DetailsCustomerComponent {
+
+}
