ed61263b68d8540ceb1fd76e7e0b32c4d94af976
ThatDRW <d.renswoude@gmail.com>
2023-03-29 09:32:47 +0200

    Feat: Updated landing page.

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+
+    isLoggedIn = false;
+
+    constructor(
+        private tokenStorage : TokenStorageService
+    ) {}
+
+    ngOnInit(): void {
+        if (this.tokenStorage.getToken())
+            this.isLoggedIn = true;
+    }
 
 }
