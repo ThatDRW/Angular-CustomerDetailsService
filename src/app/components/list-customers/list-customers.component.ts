72210f8450647be4e87d77162c80a39c5d08a3b3
ThatDRW <112971591+ThatDRW@users.noreply.github.com>
2023-04-02 15:27:42 +0200

    Impl: ErrorResponseUtilService.

--- src/app/components/list-customers/list-customers.component.ts
+++ src/app/components/list-customers/list-customers.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ErrorResponseUtilService } from 'src/app/helpers/errorresponseutil.service';
 import { Customer } from 'src/app/models/customer.model';
 import { CustomerService } from 'src/app/services/customer.service';
 
@@ -24,7 +25,8 @@ export class ListCustomersComponent implements OnInit {
 
     constructor(
         private customerService : CustomerService,
-        private router : Router
+        private router : Router,
+        private errorHelper : ErrorResponseUtilService,
     ) { }
 
     ngOnInit(): void {
@@ -37,23 +39,18 @@ export class ListCustomersComponent implements OnInit {
         this.customerService.getAllCustomers()
             .subscribe({
                 next: (res) => {
-                    console.log(res);
-
                     this.hasError = false;
                     this.errorMessage = '';
                     this.customerList = res;
 
-                    if (this.customerList) {
-                        const cust = this.customerList[0];
-                        console.log(cust);
-                    }
+                    console.log(res);
                 },
                 error: (e) => {
+                    this.hasError = true;
+                    this.errorMessage = this.errorHelper.handleError(e);
+
                     console.error(e);
                     console.log(e.error.status + " " + e.error.title);
-
-                    this.hasError = true;
-                    this.errorMessage = e.error.status + " " + e.error.title;
                 }
             });
 
@@ -65,7 +62,7 @@ export class ListCustomersComponent implements OnInit {
         this.router.navigate(["customer/get/" + data.id]);
     }
 
-    customerAddress(data: any) {
+    customerAddress(data: any) : string {
         const address = (data as Customer).address;
         return address?.streetName + " " + address?.houseNumber + ", " + address?.zipCode + " " + address?.city;
     }
