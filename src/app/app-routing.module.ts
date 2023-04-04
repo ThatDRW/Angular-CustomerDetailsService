ec0ed0d29974687caea62c06ad2ea04024b84825
ThatDRW <112971591+ThatDRW@users.noreply.github.com>
2023-04-04 15:09:32 +0200

    Fix: Removed unused imports.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LandingComponent } from './components/landing/landing.component';
 
-import { AddCustomerComponent } from './customer/components/add-customer/add-customer.component';
-import { DetailsCustomerComponent } from './customer/components/details-customer/details-customer.component';
-import { AllCustomersComponent } from './customer/components/all-customers/all-customers.component';
-import { FindCustomerComponent } from './customer/components/find-customer/find-customer.component';
-
-
 const routes: Routes = [
   { path: '', component: LandingComponent },
 ];
