b5b5033dbeeb441174f951a37e156eef7e8a1ff7
ThatDRW <112971591+ThatDRW@users.noreply.github.com>
2023-04-04 15:50:45 +0200

    Feat: Basic TokenService check and redirect.

--- src/app/core/helpers/auth.guard.ts
+++ src/app/core/helpers/auth.guard.ts
@@ -1,4 +1,25 @@
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { TokenStorageService } from "../services/token-storage.service";
+
 export const authGuard = () => {
-    console.info('AUTHGUARD CALLED');
-    return true;
+    console.info('AUTHGUARD >> Called for duty!');
+
+    console.info('AUTHGUARD >> Injecting Dependencies...');
+    const tokenStorage = inject( TokenStorageService );
+    const router = inject( Router );
+
+    console.info('AUTHGUARD >> Checking TokenStorage...');
+    let tokenFound = tokenStorage.getToken() ? true : false;
+    let tokenMsg = tokenFound ? 'was found!' : 'was not found!'
+    console.info('AUTHGUARD >> Token ' + tokenMsg);
+
+    if (tokenFound) {
+        console.info('AUTHGUARD >> Alright, cause no trouble...');
+        return true;
+    }
+
+    console.info('AUTHGUARD >> Excuse me, Sir, I need you to step out of the line.');
+    return router.parseUrl('/');
 }
