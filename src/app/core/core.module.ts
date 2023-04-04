ff5221af93021f7a8f084d754a9f0c8bae6a4197
ThatDRW <112971591+ThatDRW@users.noreply.github.com>
2023-04-04 15:09:45 +0200

    Fix: Removed unused imports.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PlaceholderHereComponent } from './components/placeholder-here/placeholder-here.component';
-
 
 
 @NgModule({
   declarations: [
-    PlaceholderHereComponent
+
   ],
   imports: [
     CommonModule
